caf3d9127f93c298459ea077392bd75a1ffe6309
Achrie <18851989124@163.com>
2025-06-30 00:38:48 +0800

    创建

---
+++ src/main/java/org/example/rocketmqjavaapi/RocketMqJavaApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.rocketmqjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketMqJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketMqJavaApiApplication.class, args);
+	}
+
+}
