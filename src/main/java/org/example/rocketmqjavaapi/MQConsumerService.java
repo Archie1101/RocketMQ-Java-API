ed15f1d83dc02b7577a7877269d01af1d6a0a2d7
Achrie <18851989124@163.com>
2025-06-30 12:00:05 +0800

    编写getLatestMessage()，在API出调用此函数返回信息

--- src/main/java/org/example/rocketmqjavaapi/MQConsumerService.java
+++ src/main/java/org/example/rocketmqjavaapi/MQConsumerService.java
@@ -8,8 +8,18 @@ import org.springframework.stereotype.Service;
 @RocketMQMessageListener(topic = "topicTest", consumerGroup = "consumer1")
 public class MQConsumerService implements RocketMQListener<String> {
 
+    // 用于保存接收到的最新消息
+    private String latestMessage;
+
     @Override
     public void onMessage(String message) {
+        // 将接收到的消息存储为最新消息
         System.out.println("接收到的消息: " + message);
+        latestMessage = message;  // 只存储最新的消息
+    }
+
+    // 提供一个方法，允许外部访问最新接收到的消息
+    public String getLatestMessage() {
+        return latestMessage;
     }
 }
